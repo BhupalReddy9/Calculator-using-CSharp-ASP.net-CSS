f78dc2e7e85ddd8ff2c57d7cc492ce92a967aa73
Bhupal Reddy Pasham <Bhupal Reddy Pasham@LAPTOP-5TV3IVCV>
2023-10-15 22:55:29 +0530

    Add project files.

---
+++ BasicCalculator/home.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="BasicCalculator.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="CSS/style.css" rel="stylesheet" />
+</head>
+<body>
+   <form id="form1" runat="server" class="Container">
+        <main>
+            <h1>Calculator</h1>
+        </main>
+        <div>
+
+            <asp:TextBox ID="Output" CssClass="textbox" placeholder="  Ans:4" ReadOnly="true" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:TextBox ID="Input" CssClass="textbox" placeholder="  Q: 2+2=?" runat="server"></asp:TextBox>
+
+        </div>
+        <div>
+            <asp:Button ID="AllClear" runat="server" Text="AC" OnClick="AllClear_Click" />
+            <asp:Button ID="Clear" runat="server" Text="C" OnClick="Clear_Click" />
+            <asp:Button ID="Delete" runat="server" Text="Del" OnClick="Delete_Click" />
+            <asp:Button ID="Divide" runat="server" Text="/" OnClick="Divide_Click" />
+        </div>
+
+        <div>
+            <asp:Button ID="seven" runat="server" Text="7" OnClick="seven_Click" />
+            <asp:Button ID="eight" runat="server" Text="8" OnClick="eight_Click" />
+            <asp:Button ID="nine" runat="server" Text="9" OnClick="nine_Click" />
+            <asp:Button ID="multiple" runat="server" Text="*" OnClick="multiple_Click" />
+        </div>
+        <div>
+            <asp:Button ID="four" runat="server" Text="4" OnClick="four_Click" />
+            <asp:Button ID="five" runat="server" Text="5" OnClick="five_Click" />
+            <asp:Button ID="six" runat="server" Text="6" OnClick="six_Click" />
+            <asp:Button ID="minus" runat="server" Text="-" OnClick="minus_Click" />
+        </div>
+        <div>
+            <asp:Button ID="one" runat="server" Text="1" OnClick="one_Click" />
+            <asp:Button ID="two" runat="server" Text="2" OnClick="two_Click" />
+            <asp:Button ID="three" runat="server" Text="3" OnClick="three_Click" />
+            <asp:Button ID="plus" runat="server" Text="+" OnClick="plus_Click" />
+        </div>
+        <div>
+            <asp:Button ID="zero" runat="server" Text="0" OnClick="zero_Click" />
+            <asp:Button ID="dot" runat="server" Text="." OnClick="dot_Click" />
+            <asp:Button ID="equalto" runat="server" Text="=" CssClass="equal" OnClick="equalto_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ BasicCalculator/home.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BasicCalculator
+{
+    public partial class home : System.Web.UI.Page
+    {
+        string ip = "", ans;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void equalto_Click(object sender, EventArgs e)
+        {
+            ip += Input.Text.ToString();
+            try
+            {
+                ans = new DataTable().Compute(ip, null).ToString();
+                Output.Text = ans.ToString();
+            }
+            catch (Exception ex)
+            {
+                Output.Text = "Wrong Expression";
+            }
+            
+
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+
+            ip += Input.Text.ToString();
+            ans = ip.Remove(ip.Length -1);
+            Input.Text = ans.ToString();
+        }
+
+
+            protected void Divide_Click(object sender, EventArgs e)
+        {
+            ip += Input.Text.ToString();
+            ip += "/";
+            Input.Text = ip.ToString();
+        }
+        protected void multiple_Click(object sender, EventArgs e)
+        {
+            ip += Input.Text.ToString();
+            ip += "*";
+            Input.Text = ip.ToString();
+        }
+        protected void plus_Click(object sender, EventArgs e)
+        {
+            ip += Input.Text.ToString();
+            ip += "+";
+            Input.Text = ip.ToString();
+        }
+
+        protected void minus_Click(object sender, EventArgs e)
+        {
+            ip += Input.Text.ToString();
+            ip += "-";
+            Input.Text = ip.ToString();
+        }
+
+
+        protected void getinput(char a)
+        {
+            ip += Input.Text.ToString();
+            ip += a;
+            Input.Text = ip.ToString();
+        }
+        protected void seven_Click(object sender, EventArgs e)
+        {
+            getinput('7');
+        }
+
+        protected void eight_Click(object sender, EventArgs e)
+        {
+            getinput('8');
+            
+        }
+
+        protected void nine_Click(object sender, EventArgs e)
+        {
+            getinput('9');
+        }
+
+        
+        protected void four_Click(object sender, EventArgs e)
+        {
+            getinput('4');
+        }
+
+        protected void five_Click(object sender, EventArgs e)
+        {
+            getinput('5');
+        }
+
+        protected void six_Click(object sender, EventArgs e)
+        {
+            getinput('6');
+        }
+
+      
+        protected void one_Click(object sender, EventArgs e)
+        {
+            getinput('1');
+        }
+
+        protected void two_Click(object sender, EventArgs e)
+        {
+            getinput('2');
+        }
+
+        protected void three_Click(object sender, EventArgs e)
+        {
+            getinput('3');
+        }
+
+       
+        protected void zero_Click(object sender, EventArgs e)
+        {
+            getinput('0');
+        }
+
+        protected void dot_Click(object sender, EventArgs e)
+        {
+            getinput('.');
+        }
+
+    
+
+        protected void AllClear_Click(object sender, EventArgs e)
+        {
+            Input.Text = String.Empty;
+            Output.Text = String.Empty;
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            Input.Text = String.Empty;
+
+        }
+    }
+}
